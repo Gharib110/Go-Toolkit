7b1912ebbec0b3f10ac1bd6fef05c91e967ed48a
Alireza Gharib <alirezagharib110@gmail.com>
2025-01-30 15:11:59 +0330

    Testing the file upload with small http server

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	mux := routes()
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/upload", uploadFiles)
+	mux.HandleFunc("/upload-one", uploadOneFile)
+
+	return mux
+}
+
+func uploadFiles(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func uploadOneFile(w http.ResponseWriter, r *http.Request) {
 
 }
