933bff5ffe6a99d428a4ed71cc6011b4fee2997f
Alireza Gharib <alirezagharib110@gmail.com>
2025-01-30 15:20:21 +0330

    upload file section is implemented

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"Go-Toolkit/toolkit"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -26,7 +28,29 @@ func routes() http.Handler {
 }
 
 func uploadFiles(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
+	t := &toolkit.Tools{
+		MaxFileSize:      1024 * 1024 * 1024,
+		AllowedFileTypes: []string{"image/jpeg", "image/png", "image/gif"},
+	}
+
+	files, err := t.UploadFiles(r, "./uploads")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	out := ""
+	for _, item := range files {
+		out += fmt.Sprintf("Uploaded %s to the uploads folder, renamed to %s\n",
+			item.OriginalFileName, item.NewFileName)
+	}
+
+	_, _ = w.Write([]byte(out))
 }
 
 func uploadOneFile(w http.ResponseWriter, r *http.Request) {
