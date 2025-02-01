fa2eebdca2ae7584d463360b157d88e1763500c6
Alireza Gharib <alirezagharib110@gmail.com>
2025-02-01 20:38:30 +0330

    WriteJSON is implemented

--- toolkit/json_tools.go
+++ toolkit/json_tools.go
@@ -44,3 +44,27 @@ func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request,
 
 	return nil
 }
+
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int,
+	data interface{}, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
